0b7250c3c080a6700ad753190810576ab1f2da5b
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-03-22 17:23:34 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# C++ TDA (_abstract data type_)
+
+* Pointers
+* Dynamic memory
+* Constructor, Getter, Setter, toString
+* Destructor (delete)
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// definitions
+
+int main()
+{
+    cout << "initial commit" << endl;
+
+    return 0;
+}
+
+// implementations
