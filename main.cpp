6dd34dc62ee05cd8239e5d28bd51b43731e77eb6
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-03-22 18:01:15 -0300

    test implementation: bank and customer

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <cstring>
 
+// TDA librarys
+#include "bank.h"
+#include "customer.h"
+
 using namespace std;
 
 // definitions
 
 int main()
 {
-    cout << "initial commit" << endl;
+    Bank b1 = constructorBank("Bank", 125);
+    toStringBank(b1); // expected output: Bank name: Bank ---- Customers amount: 125
+
+    cout << "\n" << endl; // new line
+
+    Customer c1 = constructorCustomer("Delbelt", 1111);
+    toStringCustomer(c1); // expected output: Customer name: Delbelt ---- ID: 1111
+
+    cout << "\n" << endl; // new line
 
     return 0;
 }
